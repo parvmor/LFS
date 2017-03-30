0cb0fcf1b6aa5c6ab3e2c729b165bfb79e185da3
parv <parvmor.pm@gmail.com>
2017-03-30 17:26:19 +0530

    made a separate parser

--- lancelot/blfs.py
+++ lancelot/blfs.py
@@ -40,61 +40,6 @@ def security(chapter):
         else:
             pattern = re.compile('(.*)-(\d[^\s]*)')
             name, version = re.findall(pattern,name)[0]
-        paras = parsedHtml.find_all('div', 'itemizedlist')[0].find_all('p')
-        linkFlag = 1
-        for para in paras:
-            anchors = para.find_all('a')
-            if anchors == []:
-                content = para.string.strip()
-                if re.search('MD5 Sum',content) is not None:
-                    md5sum = re.findall(':(.*)',content)[0].strip()
-                else:
-                    continue
-            elif linkFlag:
-                content = para.contents[0].strip()
-                if re.search('Download.*(([Hh][tT])|[fF])[tT][pP]',content) is not None:
-                    link = para.a['href']
-                    linkFlag = 0
-        if re.search(re.compile('[Aa]dditional\s+[Dd]ownload'),parsedHtml) is not None:
-            additionals = parsedHtml.find_all('div', 'itemizedlist')[1].find_all('a', 'ulink')
-            for additional in additionals:
-                link+=' , '
-                link += additional['href']
-        optParas = parsedHtml.find_all('p', 'optional')
-        requiredParas = parsedHtml.find_all('p', 'required') + parsedHtml.find_all('p','recommended')
-        optDeps = []
-        deps=[]
-        for opt in optParas:
-            anchors = opt.find_all('a', 'xref')
-            for anchor in anchors:
-                if re.search(pattern,anchor['title'].strip()) is None:
-                    depName = anchor['title'].strip()
-                    depVersion = 'none'
-                else:
-                    depName,depVersion = re.findall(pattern,anchor['title'].strip())[0]
-                optDeps.append((depName,depVersion))
-        for required in requiredParas:
-            anchors = required.find_all('a', 'xref')
-            for anchor in anchors:
-                if re.search(pattern,anchor['title'].strip()) is None:
-                    depName = anchor['title'].strip()
-                    depVersion = 'none'
-                else:
-                    depName,depVersion = re.findall(pattern,anchor['title'].strip())[0]
-                deps.append((depName,depVersion))
-        commands = parsedHtml.find_all('kbd', 'command')
-        for i in range(len(commands)):
-            if commands[i].string==None:
-                command = commands[i].string
-                if commands[i].find_all('em') != []:
-                    # TODO : handle em
-                    commands[i] = '\n'
-                    continue
-                commands[i] = command.contents[0]
-                commands[i] = commands[i] + command.contents[1].string
-                commands[i] = commands[i] + command.contents[2]
-            else:
-                commands[i] = commands[i].string
 
 def main():
     src = urlopen(blfs)
---
+++ lancelot/parser.py
@@ -0,0 +1,64 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup as htmlParser
+from package import package
+import re
+
+def parse(parsedHtml):
+    itemizedList = parsedHtml.find_all('div', 'itemizedlist')
+    # Download link
+    paras = itemizedList[0].find_all('p')
+    linkFlag = 1
+    pattern = re.compile('(.*)-(\d[^\s]*)')
+    for para in paras:
+        anchors = para.find_all('a')
+        if anchors == []:
+            content = para.string.strip()
+            if re.search('[Mm][Dd]5 [Ss][uU][Mm]', content) is not None:
+                md5sum = re.findall(':(.*)', content)[0].strip()
+            else:
+                continue
+            elif linkFlag:
+                content = para.contents[0].sttip()
+                if re.search('[Dd]ownload.* (([Hh][Tt])|[Ff])[tT][pP]', content) is not None:
+                    link = para.a['href']
+                    linkFlag = 0
+    if re.search(re.compile('[Aa]dditional\s+[Dd]ownload'), parsedHtml) is not None:
+        additionals = itemizedList[1].find_all('a', 'ulink')
+        for additional in additionals:
+            link += ' , '
+            link += additional['href']
+    optParas = parsedHtml.find_all('p', 'optional')
+    requiredParas = parsedHtml.find_all('p', 'required') + parsedHtml.find_all('p', 'recommended')
+    optDeps = []
+    deps = []
+    for opt in optParas:
+        anchors = opt.find_all('a', 'xref')
+        for anchor in anchors:
+            if re.search(pattern, anchor['title'].strip()) is None:
+                depName = anchor['title'].strip()
+                depVersion = 'none'
+            else:
+                depName, depVersion = re.findall(pattern, anchor['title'].strip())[0]
+            optDeps.append((depName, depVersion))
+    for required in requiredParas:
+        anchors = required.find_all('a', 'xref')
+        for anchor in anchors:
+            if re.search(pattern, anchor['title'].strip()) is None:
+                depName = anchor['title'].strip()
+                depVersion = 'none'
+            else:
+                depName, depVersion = re.findall(pattern, anchor['title'].strip())[0]
+            deps.append((depName, depVersion))
+    commands = parsedHtml.find_all('kbd', 'command')
+    for i in range(len(commands)):
+        if commands[i].string is None:
+            command = commands[i]
+            if commands[i].find_all('em') != []:
+                # TODO  : handle em
+                commands[i] ='\n'
+                continue
+            commands[i] = command.contents[0]
+            commands[i] = commands[i] + command.contents[1].string
+            commands[i] = commands[i] + command.contents[2]
+        else:
+            commands[i] = commands[i].string
