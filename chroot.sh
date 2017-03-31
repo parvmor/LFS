d34e9056ca619fd293861f35270c16bfcac2c1de
parv <parvmor.pm@gmail.com>
2017-04-01 04:03:28 +0530

    added lancelot to path

--- chroot.sh
+++ chroot.sh
@@ -1,4 +1,4 @@
 sudo chroot "$LFS" /usr/bin/env -i              \
     HOME=/root TERM="$TERM" PS1='\u:\w\$ ' \
-    PATH=/bin:/usr/bin:/sbin:/usr/sbin     \
+    PATH=/bin:/usr/bin:/sbin:/usr/sbin:/lancelot     \
     /bin/bash --login
